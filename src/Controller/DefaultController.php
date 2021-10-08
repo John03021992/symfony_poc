c4abcb2a82fe5d4a19bda3da971fe63f443b281d
John <jonathan.juillerat@skin-soft.org>
2021-10-08 16:53:39 +0200

    login redirection and style ok, working on access control security

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -35,7 +35,6 @@ class DefaultController extends AbstractController
     public function index2(): Response
     {
         
-
        
 
         return $this->render('default/index2.html.twig', [
@@ -44,29 +43,6 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    #[Route('/index3', name: 'index3')]
-    public function index3(): Response
-    {
-       
-
-      
-
-        return $this->render('default/index3.html.twig', [
-            'controller_name' => 'DefaultController',
-            'param2' => $this->service->test(),
-        ]);
-    }
-
-    #[Route('/index4', name: 'index4')]
-    public function index4(): Response
-    {
-        dump('test');
-
-        return $this->render('default/index4.html.twig', [
-            'controller_name' => 'DefaultController',
-            'param2' => $this->service->test(),
-        ]);
-    }
 
     #[Route('/api', name: 'api')]
     public function api(): Response
